2e869eeb58ab5012635eca037b7a8e01b632df1c
Randolph Xia <xia.randolph.227@gmail.com>
2018-06-14 00:00:08 -0400

    Add SFML to CMake
    Really hard to do on linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
-#include <stdio.h>
+#include <SFML/Graphics.hpp>
 
 int main(int argc, char* argv[]) {
-    printf("minecraft-cpp: running\n");
+  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Test");
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Green);
 
-    return 0;
+  while (window.isOpen()) {
+
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    window.clear();
+    window.draw(shape);
+    window.display();
+  }
+
+  return 0;
 }
